ebcf1b980730d59f3edeb03c9cdef46a31fca54f
Samuel Cochran <sj26@sj26.com>
2017-07-19 10:44:38 +1000

    Use gist with git credential support

---
+++ Formula/gist.rb
@@ -0,0 +1,13 @@
+class Gist < Formula
+  desc "Command-line utility for uploading Gists"
+  homepage "https://github.com/defunkt/gist"
+  head "https://github.com/sj26/gist.git", branch: "auth-token-from-git-credential-helper"
+
+  def install
+    rake "install", "prefix=#{prefix}"
+  end
+
+  test do
+    assert_match %r{https:\/\/gist}, pipe_output("#{bin}/gist", "homebrew")
+  end
+end
