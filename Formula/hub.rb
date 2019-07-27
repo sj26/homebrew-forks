a594039cc82353fa4926e8dc4127be560fa5b52a
Samuel Cochran <sj26@sj26.com>
2019-07-27 16:45:00 +1000

    Add fork of hub

---
+++ Formula/hub.rb
@@ -0,0 +1,29 @@
+class Hub < Formula
+  desc "Add GitHub support to git on the command-line"
+  homepage "https://hub.github.com/"
+  head "https://github.com/sj26/hub.git", branch: "git-credentials"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/github/hub").install buildpath.children
+    cd "src/github.com/github/hub" do
+      system "make", "install", "prefix=#{prefix}"
+
+      prefix.install_metafiles
+
+      bash_completion.install "etc/hub.bash_completion.sh"
+      zsh_completion.install "etc/hub.zsh_completion" => "_hub"
+      fish_completion.install "etc/hub.fish_completion" => "hub.fish"
+    end
+  end
+
+  test do
+    system "git", "init"
+    %w[haunted house].each { |f| touch testpath/f }
+    system "git", "add", "haunted", "house"
+    system "git", "commit", "-a", "-m", "Initial Commit"
+    assert_equal "haunted\nhouse", shell_output("#{bin}/hub ls-files").strip
+  end
+end
